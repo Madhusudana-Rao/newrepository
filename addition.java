43974a2e33f3c55bc37922802e9da1b79e5ebac9
Madhusudana Rao <53528070+Madhusudana-Rao@users.noreply.github.com>
2021-10-08 09:38:51 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class addition{
+  public static void main(String[] args){
+  int a,b,c;
+  Scanner sc=new Scanner(System.in);
+  a=sc.nextInt();
+  b=sc.nextInt();
+  c=a+b;
+    System.out.println("The sum is : "+c);
+    System.out.println("GIthub Repository !!");
+  }
+}
+  
+  
+  
